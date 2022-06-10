229fee24bdd821c33a1add1ffd28621fd8f563e7
mludwig99 <michalludwig99@gmail.com>
2022-06-10 11:49:57 +0200

    39-dlugosc-stringa

---
+++ script/main.js
@@ -0,0 +1,9 @@
+let txt1 = 'Uwielbiam JavaScript';
+let txt2 = 'Jestem świetnym programistą'
+
+function result(a, b) {
+    let textFinal = a + ", " + b
+    txt2;
+    return textFinal;
+}
+console.log(result(txt1, txt2))
